b8edf6d1806921ba06e5366f4b8396eadac3e0bd
Ryan McCauley <ryanmccauley211@gmail.com>
2018-12-09 10:36:21 +0000

    - Remove method generatePage (only used for testing) - Add padding to pages - Change inset methods to offset method to prevent auto-calibrating - Rename properties

--- Assets/Scripts/DynamicPageController.cs
+++ Assets/Scripts/DynamicPageController.cs
@@ -1,51 +1,34 @@
 ﻿using System;
+using Ereader;
 using TMPro;
 using UnityEngine;
 
 public class DynamicPageController : MonoBehaviour {
 
-    public BookPro book; 
-    private float margin;
+    public BookPro bookCurlWrapper; 
 
-    public void generatePage(String pageFrontText, String pageBackText){
-        GameObject frontObj = new GameObject("pageFront");
-        GameObject backObj = new GameObject("pageBack");
-        
-        TextMeshProUGUI frontTMP = frontObj.AddComponent<TextMeshProUGUI>();
-        TextMeshProUGUI backTMP = backObj.AddComponent<TextMeshProUGUI>();
-
-        frontTMP.text = pageFrontText;
-        backTMP.text = pageBackText;
-        
-        AddPaper(frontTMP, backTMP);
-        fitTMP(frontTMP);
-        fitTMP(backTMP);
-    }
-
-    public void fitTMP(TextMeshProUGUI textMeshProUgui) {
+    public void fitTMP(TextMeshProUGUI textMeshProUgui, 
+        float paddingTop, float paddingBottom, float paddingLeft, float paddingRight) {
         RectTransform rectTransform = textMeshProUgui.GetComponent<RectTransform>();
 
-        rectTransform.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, margin, 0);
-        rectTransform.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Right, margin, 0);
-        rectTransform.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Bottom, margin, 0);
-        rectTransform.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, margin, 0);
-        
+        rectTransform.offsetMax = new Vector2(-paddingLeft, -paddingTop);
+        rectTransform.offsetMin = new Vector2(paddingRight, paddingBottom);
         rectTransform.anchorMin = new Vector2(0, 0);
         rectTransform.anchorMax = new Vector2(1, 1);
     }
     
     public void AddPaper(TextMeshProUGUI front, TextMeshProUGUI back){
 
-        int pageNum = book.papers.Length * 2;
+        int pageNum = bookCurlWrapper.papers.Length * 2;
         
         GameObject pagePrefab = (GameObject)Resources.Load("prefabs/PageBlank", typeof(GameObject));
-        GameObject frontPage = Instantiate(pagePrefab, new Vector3(0, 0, 0), Quaternion.identity, book.transform);
+        GameObject frontPage = Instantiate(pagePrefab, new Vector3(0, 0, 0), Quaternion.identity, bookCurlWrapper.transform);
         frontPage.name = "page" + pageNum;
-        GameObject backPage = Instantiate(pagePrefab, new Vector3(0, 0, 0), Quaternion.identity, book.transform);
+        GameObject backPage = Instantiate(pagePrefab, new Vector3(0, 0, 0), Quaternion.identity, bookCurlWrapper.transform);
         backPage.name = "page" + (pageNum + 1);
         
-        frontPage.transform.SetParent(book.transform, false);
-        backPage.transform.SetParent(book.transform, false);
+        frontPage.transform.SetParent(bookCurlWrapper.transform, false);
+        backPage.transform.SetParent(bookCurlWrapper.transform, false);
         
         Paper newPaper = new Paper();
         newPaper.Front = frontPage;
@@ -53,14 +36,14 @@ public class DynamicPageController : MonoBehaviour {
         front.transform.parent = newPaper.Front.transform;
         back.transform.parent = newPaper.Back.transform;
         
-        Paper[] papers = new Paper[book.papers.Length + 1];
-        for(int i=0; i < book.papers.Length ;i++) {
-            papers[i] = book.papers[i];
+        Paper[] papers = new Paper[bookCurlWrapper.papers.Length + 1];
+        for(int i=0; i < bookCurlWrapper.papers.Length ;i++) {
+            papers[i] = bookCurlWrapper.papers[i];
         }
         papers[papers.Length-1] = newPaper;
         
-        book.papers = papers;
-        book.EndFlippingPaper = book.papers.Length - 1;
-        book.UpdatePages();
+        bookCurlWrapper.papers = papers;
+        bookCurlWrapper.EndFlippingPaper = bookCurlWrapper.papers.Length - 1;
+        bookCurlWrapper.UpdatePages();
     }
 }
