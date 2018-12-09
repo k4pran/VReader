4aeb781fb2e4dfcfd363b871b9b4e80941e0ab04
Ryan McCauley <ryanmccauley211@gmail.com>
2018-12-09 11:57:21 +0000

    - Add all pages in book to bookCurlPro - Add listener for when page is changed - Add todo (enable only pages currently on display)

--- Assets/Modules/Ereader/EReaderBridge.cs
+++ Assets/Modules/Ereader/EReaderBridge.cs
@@ -6,16 +6,28 @@ namespace Ereader{
 
         public BookPro bookPro;
         public DynamicPageController dynamicPageController;
+        private Book book;
         
         void Start(){
-            
+                        
             BasicBook book = new BasicBook("Assets/Resources/Books/dracula.txt");
             book.LoadBook();
-            dynamicPageController.AddPaper(book.Pages[0].Tmp, book.Pages[1].Tmp);
-            dynamicPageController.fitTMP(book.Pages[0].Tmp, 
-                book.PaddingTop, book.PaddingBottom, book.PaddingLeft, book.PaddingRight);
-            dynamicPageController.fitTMP(book.Pages[1].Tmp, 
-                book.PaddingTop, book.PaddingBottom, book.PaddingLeft, book.PaddingRight);
+            bookPro.OnFlip.AddListener(pageChanged);
+
+            for(int i = 1; i < book.PageCount(); i += 2){
+                
+                dynamicPageController.AddPaper(book.Pages[i - 1].Tmp, book.Pages[i].Tmp);
+                
+                dynamicPageController.fitTMP(book.Pages[i - 1].Tmp, 
+                    book.PaddingTop, book.PaddingBottom, book.PaddingLeft, book.PaddingRight);
+                dynamicPageController.fitTMP(book.Pages[i].Tmp, 
+                    book.PaddingTop, book.PaddingBottom, book.PaddingLeft, book.PaddingRight);
+            }  
+        }
+
+        // todo enable only pages currently displayed
+        public void pageChanged(){
+            Debug.Log((bookPro.CurrentPaper - 1) * 2);
         }
 
         private void Update(){
