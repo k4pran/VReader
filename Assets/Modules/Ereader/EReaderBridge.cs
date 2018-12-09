1a10af8225ddb02bd6e48d3f0b353896191375fc
Ryan McCauley <ryanmccauley211@gmail.com>
2018-12-09 10:41:27 +0000

    - Add EReader bridge to bridge ereader functionality with Unity

---
+++ Assets/Modules/Ereader/EReaderBridge.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Ereader{
+    
+    public class EReaderBridge : MonoBehaviour{
+
+        public BookPro bookPro;
+        public DynamicPageController dynamicPageController;
+        
+        void Start(){
+            
+            BasicBook book = new BasicBook("Assets/Resources/Books/dracula.txt");
+            book.LoadBook();
+            dynamicPageController.AddPaper(book.Pages[0].Tmp, book.Pages[1].Tmp);
+            dynamicPageController.fitTMP(book.Pages[0].Tmp, 
+                book.PaddingTop, book.PaddingBottom, book.PaddingLeft, book.PaddingRight);
+            dynamicPageController.fitTMP(book.Pages[1].Tmp, 
+                book.PaddingTop, book.PaddingBottom, book.PaddingLeft, book.PaddingRight);
+        }
+
+        private void Update(){
+            
+        }
+    }
+}
