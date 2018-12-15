2fe57034b499392f1c105dcf6aa5fa589ae709e7
Ryan McCauley <ryanmccauley211@gmail.com>
2018-12-15 08:47:13 +0000

    - Remove event handlers - TextEventHandler property added - TextEventListener propert added and attached to handler

--- Assets/Modules/Ereader/Page.cs
+++ Assets/Modules/Ereader/Page.cs
@@ -4,38 +4,32 @@ using UnityEngine;
 
 namespace Ereader{
     [Serializable] 
-    public class Page{
+    public class Page {
             
         private TextMeshProUGUI tmp;
-        private TMP_TextEventHandler textHandler;
+        private TextEventListener textEventListener;
+        private TextEventHandler textEventHandler;
         private string objName;
         private int pageNum;
 
         private Vector2 preferredVals;
 
-        public Page(TextMeshProUGUI tmp, TMP_TextEventHandler textHandler, string objName, int pageNum){
+        public Page(TextMeshProUGUI tmp, string objName, int pageNum){
             this.tmp = tmp;
             this.tmp.enableAutoSizing = true;
             this.tmp.fontSizeMin = 12;
             this.tmp.color = Color.black;
-            this.textHandler = textHandler;
             this.objName = objName;
             this.pageNum = pageNum;
             
+            textEventListener = tmp.gameObject.AddComponent<TextEventListener>();
+            
             Disable();
+            SetHandlers();
         }
 
         private void SetHandlers(){
-            textHandler.onWordSelection.AddListener(WordHovered);
-            textHandler.onMouseHoldClick.AddListener(WordFocused);
-        }
-
-        private void WordHovered(string word, int x, int y){
-            Debug.Log("word " + word + " selected");
-        }
-
-        private void WordFocused(string word, int x, int y){
-            Debug.Log("Word = " + word);
+            textEventHandler.textEventListener = textEventListener;
         }
 
         public void Update() {
@@ -44,12 +38,12 @@ namespace Ereader{
 
         public void Disable(){
             tmp.enabled = false;
-            textHandler.enabled = false;
+            textEventListener.enabled = false;
         }
 
         public void Enable() {
             tmp.enabled = true;
-            textHandler.enabled = true;
+            textEventListener.enabled = true;
         }
 
         public string GetText(){
@@ -59,10 +53,9 @@ namespace Ereader{
         public static Page GetBlank(){
             GameObject gameObj = new GameObject("BlankTMP");
             gameObj.AddComponent<TextMeshProUGUI>();
-            TMP_TextEventHandler textHandler = gameObj.AddComponent<TMP_TextEventHandler>();
             TextMeshProUGUI tmp = gameObj.GetComponent<TextMeshProUGUI>();
             tmp.text = "";
-            return new Page(tmp, textHandler, "BlankTMP", -1);
+            return new Page(tmp, "BlankTMP", -1);
         }
 
         public Vector2 PreferredVals{
