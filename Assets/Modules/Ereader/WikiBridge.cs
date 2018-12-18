76bff89c500f19ff93458a7b77e6f525bfb1d347
Ryan McCauley <ryanmccauley211@gmail.com>
2018-12-18 10:57:57 +0000

    - Connect wiki queries to unity - Change method from WebRequest to lower level WWW due to auth issues - Create WikiBridge class to use wiki results in unity - Add utility classes to help build API requests

---
+++ Assets/Modules/Ereader/WikiBridge.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Ereader.Knowledge;
+using UnityEngine;
+using WikiQueries;
+
+namespace Ereader{
+    
+    /**
+     * Bridges wikipedia API results and renders them in unity
+     */
+        
+    public class WikiBridge {
+        
+        private static GameObject wiki;
+
+        public static void LogResults(String selection) {
+            if (wiki == null) {
+                wiki = new GameObject();
+                wiki.AddComponent<Wiki>();
+            }
+
+            wiki.GetComponent<Wiki>().PageSummary(selection);
+        }
+    }
+}
--- Assets/Modules/Wiki/Wiki.cs
+++ Assets/Modules/Wiki/Wiki.cs
@@ -1,101 +1,90 @@
 ﻿using System;
+using System.Collections;
+using System.IO;
 using System.Net;
-using System.Net.Security;
-using System.Security.Cryptography.X509Certificates;
-using Utils;
+using UnityEngine;
 
-namespace Ereader.Knowledge{
-    public class Wiki{
+namespace WikiQueries{
+    
+    public class Wiki : MonoBehaviour {
 
-        public void QueryPageInfo(string query){
+        private static string BASE_URI = "https://en.wikipedia.org/w/api.php";
+        
+        public string QueryPageName(QueryBuilder query){
             
-            ServicePointManager.ServerCertificateValidationCallback += new RemoteCertificateValidationCallback((sender, certificate, chain, policyErrors) => { return true; });
+            WWW www = new WWW(query.ToString());
+            StartCoroutine(WaitForRequest(www));
             
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(
-                "https://en.wikipedia.org/w/api.php?action=query&prop=categories|info|description|images|info|links&format=xmlfm&titles=" + query);//
-            request.Method = "GET";
+            return "";
+        }
 
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-            string myResponse = "";
-            using (System.IO.StreamReader sr = new System.IO.StreamReader(response.GetResponseStream())){
-                myResponse = sr.ReadToEnd();
+        /// <summary>
+        /// Grab the first paragraph of a page
+        /// </summary>
+        /// <param name="pageTitle">Title of wiki page</param>
+        /// <param name="format">Format to retrieve</param>
+        /// <param name="plainText">Retrieve as plain text or with html tags</param>
+        /// <returns></returns>
+        public string PageSummary(string pageTitle, Format format=Format.XML, bool plainText=false){
+            QueryBuilder query = new QueryBuilder(BASE_URI, format);
+            
+            query.AppendParam("action", "query");
+            query.AppendParam("titles", pageTitle);
+            query.AppendParam("prop", "extracts");
+            query.AppendParam("exintro");
+            if (plainText){
+                query.AppendParam("explaintext");
             }
-            Console.Write("");
+
+            query.Format = Format.XML;
+            string result = QueryPageName(query);
+            return result;
+        }
+
+        /// <summary>
+        /// Check a page exists
+        /// </summary>
+        /// <param name="pageTitle">Title of wiki page</param>
+        /// <returns></returns>
+        public bool DoesPageExist(string pageTitle){
+            QueryBuilder query = new QueryBuilder(BASE_URI, Format.JSON);
+            
+            query.AppendParam("action", "query");
+            query.AppendParam("titles", pageTitle);
+            query.Format = Format.XML;
+            string result = QueryPageName(query);
+            
+            ParserXml parserXml = new ParserXml(result);
+            string idStr = parserXml.PageId();
+            try{
+                int id = Int32.Parse(idStr);
+                if (id >= 0){
+                    return true;
+                }
+            }
+            catch(FormatException e){
+                return false;
+            }
+            return false;
         }
         
-
-        private static bool RemoteCertificateValidationCallback(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        private IEnumerator WaitForRequest(WWW www) {
+            yield return www;
+     
+            // check for errors
+            if (www.error == null)
             {
-                //Return true if the server certificate is ok
-                if (sslPolicyErrors == SslPolicyErrors.None)
-                    return true;
-        
-                bool acceptCertificate = true;
-                string msg = "The server could not be validated for the following reason(s):\r\n";
-        
-                //The server did not present a certificate
-                if ((sslPolicyErrors &
-                     SslPolicyErrors.RemoteCertificateNotAvailable) == SslPolicyErrors.RemoteCertificateNotAvailable)
-                {
-                    msg = msg + "\r\n    -The server did not present a certificate.\r\n";
-                    acceptCertificate = false;
-                }
-                else
-                {
-                    //The certificate does not match the server name
-                    if ((sslPolicyErrors &
-                         SslPolicyErrors.RemoteCertificateNameMismatch) == SslPolicyErrors.RemoteCertificateNameMismatch)
-                    {
-                        msg = msg + "\r\n    -The certificate name does not match the authenticated name.\r\n";
-                        acceptCertificate = false;
-                    }
-        
-                    //There is some other problem with the certificate
-                    if ((sslPolicyErrors &
-                         SslPolicyErrors.RemoteCertificateChainErrors) == SslPolicyErrors.RemoteCertificateChainErrors)
-                    {
-                        foreach (X509ChainStatus item in chain.ChainStatus)
-                        {
-                            if (item.Status != X509ChainStatusFlags.RevocationStatusUnknown &&
-                                item.Status != X509ChainStatusFlags.OfflineRevocation)
-                                break;
-        
-                            if (item.Status != X509ChainStatusFlags.NoError)
-                            {
-                                msg = msg + "\r\n    -" + item.StatusInformation;
-                                acceptCertificate = false;
-                            }
-                        }
-                    }
-                }
-        
-                //If Validation failed, present message box
-                if (acceptCertificate == false)
-                {
-                    msg = msg + "\r\nDo you wish to override the security check?";
-        //          if (MessageBox.Show(msg, "Security Alert: Server could not be validated",
-        //                       MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1) == DialogResult.Yes)
-                        acceptCertificate = true;
-                }
-        
-                return acceptCertificate;
-            }
+                Debug.Log("WWW Success: " + www.text);
+            } else {
+                Debug.Log("WWW Error: " + www.error);
+            }    
+        }
     }
     
+    
+    
+    class QueryResponseException : Exception{
+        public QueryResponseException(string message) : base(message){
+        }
+    }
 }
-
-
-// WIKIPEDIA API
-
-//Useful page info (prop aka properties):
-//categories
-//description
-//extracts (returns html or plain text)
-//images
-//info
-//Links
-//e.g. https://en.wikipedia.org/w/api.php?action=query&prop=categories|info|description|images|info|links&format=xmlfm&titles=snow
-//NOTE: If page doesn’t exist it will have a negative id (_idx)
-//Get page content
-//https://en.wikipedia.org/w/api.php?action=query&format=xmlfm&prop=revisions&titles=snow&rvprop=content&rvslots=*&rvlimit=1
-//
