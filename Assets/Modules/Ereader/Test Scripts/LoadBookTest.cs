f4fa133401e5043cb766895fa477fedfcdaf44bd
Ryan McCauley <ryanmccauley211@gmail.com>
2019-01-03 10:16:42 +0000

    Add test scenes
    - Test page curls
    - Test importing books
    - Test loading books into application (include script to assist this test)

---
+++ Assets/Modules/Ereader/Test Scripts/LoadBookTest.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Ereader.Test_Scripts {
+    
+    public class LoadBookTest : MonoBehaviour{
+
+        public EReaderBridge eReaderBridge;
+
+        private void Start() {
+            string bookPath = "Assets/Resources/Books/dracula.txt";
+
+            GameObject importerGameObject = new GameObject();
+            BookImporter bookImporter = importerGameObject.AddComponent<BookImporter>();
+            bookImporter.Import(bookPath);
+            eReaderBridge.InitBook("dracula");
+        }
+    }
+}
